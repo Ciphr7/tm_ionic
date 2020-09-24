11b7b55fa28e2a0ca10fadc7ea729df9d7b1ce0f
Kutan <gilgarcia35@gmail.com>
2020-09-24 02:12:04 -0500

    added geo location

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { MenuController, Platform } from '@ionic/angular';
 import { HomePage } from '../home/home.page';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-menu',
@@ -12,7 +12,7 @@ export class MenuPage implements OnInit {
   paneEnabled = true;
   value: string ="";
 
-  constructor(private menu: MenuController, private geolocation: Geolocation, private ptl:Platform ){ }
+  constructor(private menu: MenuController, private geolocation:Geolocation, private plt:Platform) { }
 
   openFirst(){
     this.menu.enable(true, 'first');
@@ -37,11 +37,53 @@ export class MenuPage implements OnInit {
      if (!this.value) {
 
        this.value=("")
-     } else loadPosition() {
-      this.plt.ready().then(() => {
-        this.geolocation.getCurrentPosition().then(resp => {
-          const lat = resp.coords.latitude;
-          const lng = resp.coords.longitude;
-          this.value = (lat, lng);
-          
-        )} 
+     } else this.loadUserPosition()
+   }
+         
+  loadUserPosition(){
+    this.plt.ready().then(() => {
+      this.geolocation.getCurrentPosition().then(resp => {
+        const lat = resp.coords.latitude;
+        const lng = resp.coords.longitude;
+        this.value = (lat + ':' + lng); 
+           });
+    });
+  }
+     
+   }
+
+  //  setOrg(){
+  //   let b = Boolean;
+  //   if (!b) {
+  //     this.value=("");
+
+  //   } else this.value=("hello");
+  // };
+
+
+//   setGPSHome(){
+//     let b = ("setGPShome").slice("checked");
+//     if (!b) {
+//       $("#origin").val("");
+
+//   } else if (navigator.geolocation) {
+//       var options = {
+//           maximumAge: 0,
+//           timeout:30000,
+//           enableHighAccuracy: true};
+//       navigator.geolocation.getCurrentPosition(success, error, [options]);
+
+//   } else {
+//       alert("HTML5 Not supported");
+//   }
+// }
+//   error(err) {
+//     console.warn(`ERROR(${err.code}): ${err.message}`);
+//   }
+//   success(position) {
+//     var lat = position.coords.latitude;
+//     var lon = position.coords.longitude;
+//     $("#origin").val(lat + ':' + lon);
+
+//   }
+//}
